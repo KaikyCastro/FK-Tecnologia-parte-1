b967d6849b2fef1a1137f42776a95d6689c7c077
KaikyCastro <kaiky.r.castro@gmail.com>
2025-08-28 08:44:38 -0300

    Reiniciando o arquivo CRUD.py

--- src/Models/CRUD.py
+++ src/Models/CRUD.py
@@ -1,37 +1,3 @@
-from database import BancoDeDados
+from database import consulta
+import customtkinter
 
-class CRUD:
-    def __init__(self):
-        None
-
-    def inserir(self, nome, preco):
-        self.db = BancoDeDados()
-        self.conexao = self.db.conectar()
-
-        self.cursor = self.conexao.cursor()
-        self.cursor.execute('INSERT INTO produtos (nome, preco) VALUES (%s, %s)')
-        self.conexao.commit()
-        self.cursor.close()
-        self.db.desconectar()
-
-    def alterar(self, id, nome, preco):
-        self.db = BancoDeDados()
-        self.conexao = self.db.conectar()
-
-        self.cursor = self.conexao.cursor()
-        self.cursor.execute('UPDATE produtos SET nome = %s, preco = %s WHERE id = %s', (nome, preco, id))
-        self.conexao.commit()
-        self.cursor.close()
-        self.db.desconectar()
-
-    def pesquisar(self, nome):
-        None
-
-    def remover(self):
-        None
-
-    def listar(self):
-        None
-    
-    def exibir(self):
-        None
