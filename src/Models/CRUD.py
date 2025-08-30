8af9d4724462662c9671bd66f8f75abe4526d30b
KaikyCastro <kaiky.r.castro@gmail.com>
2025-08-30 18:22:00 -0300

    Testando colocar a imagem no front

--- src/Models/CRUD.py
+++ src/Models/CRUD.py
@@ -1,3 +1,16 @@
-from database import consulta
+from PIL import Image
 import customtkinter
 
+app = customtkinter.CTk()
+app.geometry("400x300")
+app.title("FK Tecnologia")
+
+imagem = customtkinter.CTkImage(light_image=Image.open("assets/images/logo.jpeg"),
+                                dark_image=Image.open("assets/images/logo.jpeg"),
+                                size=(200, 150))
+
+label_imagem = customtkinter.CTkLabel(app, image=imagem, text="")
+label_imagem.pack(pady=20)
+
+
+app.mainloop()
