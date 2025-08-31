2db10067be5a63118d2cc258296688e1e57e469f
KaikyCastro <kaiky.r.castro@gmail.com>
2025-08-31 16:23:26 -0300

    Adicionado um pouco do front

--- src/Models/CRUD.py
+++ src/Models/CRUD.py
@@ -1,16 +1,7 @@
 from PIL import Image
-import customtkinter
+from front import FrontModel
 
-app = customtkinter.CTk()
-app.geometry("400x300")
-app.title("FK Tecnologia")
-
-imagem = customtkinter.CTkImage(light_image=Image.open("assets/images/logo.jpeg"),
-                                dark_image=Image.open("assets/images/logo.jpeg"),
-                                size=(200, 150))
-
-label_imagem = customtkinter.CTkLabel(app, image=imagem, text="")
-label_imagem.pack(pady=20)
-
+app = FrontModel()
 
 app.mainloop()
+    
---
+++ src/Models/front.py
@@ -0,0 +1,24 @@
+import customtkinter
+from PIL import Image
+
+class FrontModel(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1280x720")
+        self.title("FK Tecnologia")
+        self.resizable(False, False)
+        self._set_appearance_mode("light")
+        self.tela_inicial()
+        self.botao_proximo()
+
+
+    def tela_inicial(self):
+        self.label = customtkinter.CTkLabel(self, text="Bem vindo a FK Tecnologia", font=("Arial", 30), text_color="#000000", fg_color="#FFFFFF")
+        self.label.pack(pady=20)
+        self.image = customtkinter.CTkImage(Image.open("assets/images/logo.jpeg"), size=(760, 570))
+        self.logo = customtkinter.CTkLabel(self, image=self.image, text="")
+        self.logo.pack(pady=10)
+        
+    def botao_proximo(self):
+        self.button = customtkinter.CTkButton(self, text="Próximo", font=("Arial", 20), fg_color="#FFFFFF", hover_color="#785252", text_color="#060505", bg_color="#FFFFFF" )
+        self.button.place(x=1100, y=600)
